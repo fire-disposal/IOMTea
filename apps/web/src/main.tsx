8dd76d782af4979d8d966d4198eb0183fd884889
FireDisposal <3295829485@qq.com>
2026-05-11 21:55:37 +0800

    fix: remove broken @mantine/modals/styles.css import (not exported in v8)

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -6,7 +6,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import '@mantine/core/styles.css'
 import '@mantine/notifications/styles.css'
-import '@mantine/modals/styles.css'
 import { App } from './App'
 import { getTrpcClient, trpc } from './trpc'
 
