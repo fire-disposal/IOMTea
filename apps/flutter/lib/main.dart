152faa665321aef69b60a4b768ee5534c5e91c97
fire-disposal <3295829485@qq.com>
2026-05-20 14:47:34 +0800

    feat: register PoseMode and DetectMode in app startup

--- apps/flutter/lib/main.dart
+++ apps/flutter/lib/main.dart
@@ -2,10 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'app.dart';
 import 'services/pin_service.dart';
+import 'services/vision/vision_mode_registry.dart';
+import 'services/vision/modes/pose_mode.dart';
+import 'services/vision/modes/detect_mode.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await PinService.instance.loadSavedPin();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
+  VisionModeRegistry.register(PoseMode());
+  VisionModeRegistry.register(DetectMode());
+
   runApp(const IomteaToolsApp());
 }
