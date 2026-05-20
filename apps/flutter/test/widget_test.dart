c384a3defbf79097813301a922fe6d39170adbdf
fire-disposal <3295829485@qq.com>
2026-05-20 14:48:22 +0800

    test: register vision modes in test setup

--- apps/flutter/test/widget_test.dart
+++ apps/flutter/test/widget_test.dart
@@ -2,8 +2,16 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:iomtea_tools/app.dart';
 import 'package:iomtea_tools/services/pin_service.dart';
+import 'package:iomtea_tools/services/vision/vision_mode_registry.dart';
+import 'package:iomtea_tools/services/vision/modes/pose_mode.dart';
+import 'package:iomtea_tools/services/vision/modes/detect_mode.dart';
 
 void main() {
+  setUp(() {
+    VisionModeRegistry.register(PoseMode());
+    VisionModeRegistry.register(DetectMode());
+  });
+
   testWidgets('App renders PIN screen by default', (WidgetTester tester) async {
     SharedPreferences.setMockInitialValues({});
     await PinService.instance.loadSavedPin();
