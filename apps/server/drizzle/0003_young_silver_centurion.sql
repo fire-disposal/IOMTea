85bdf66f355dfb510f406a456e37acb7d95339cf
FireDisposal <3295829485@qq.com>
2026-05-24 20:21:18 +0800

    db: remove orphaned migration file

--- apps/server/drizzle/0003_young_silver_centurion.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE "devices" DISABLE ROW LEVEL SECURITY;--> statement-breakpoint
-DROP TABLE "devices" CASCADE;--> statement-breakpoint
-ALTER TABLE "events" DROP CONSTRAINT "events_device_id_devices_id_fk";
---> statement-breakpoint
-DROP INDEX "events_device_time_idx";--> statement-breakpoint
-ALTER TABLE "users_pin" ADD COLUMN "description" text DEFAULT '';--> statement-breakpoint
-ALTER TABLE "events" DROP COLUMN "device_id";--> statement-breakpoint
-ALTER TABLE "users_pin" DROP COLUMN "thing_id";
