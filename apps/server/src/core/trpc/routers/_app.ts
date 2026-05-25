fddb73f9d81b5c5e2d40898b00d4d6ed8c4c61c3
FireDisposal <3295829485@qq.com>
2026-05-25 16:57:46 +0800

    refactor(trpc): remove checklist/credit/streak/plan/health-records routers

--- apps/server/src/core/trpc/routers/_app.ts
+++ apps/server/src/core/trpc/routers/_app.ts
@@ -2,19 +2,15 @@ import { router } from '../index'
 import { alertRouter } from './alert'
 import { alertRuleRouter } from './alertRule'
 import { authRouter } from './auth'
-import { checklistRouter } from './checklist'
-import { creditRouter } from './credit'
 import { dashboardRouter } from './dashboard'
 import { dataRouter } from './data'
 import { exportRouter } from './export'
-import { healthRecordsRouter } from './health-records'
 import { homeGraphRouter } from './home-graph'
+import { ingestRouter } from './ingest'
 import { medicationRouter } from './medication'
 import { nodeGraphRouter } from './node-graph'
 import { patientRouter } from './patient'
 import { pinRouter } from './pin'
-import { planRouter } from './plan'
-import { streakRouter } from './streak'
 import { tagRouter } from './tag'
 import { userRouter } from './user'
 import { virtualPinRouter } from './virtual-pin'
@@ -26,21 +22,17 @@ export const appRouter = router({
   alert: alertRouter,
   alertRule: alertRuleRouter,
   auth: authRouter,
-  checklist: checklistRouter,
-  credit: creditRouter,
   dashboard: dashboardRouter,
   data: dataRouter,
   export: exportRouter,
-  healthRecords: healthRecordsRouter,
   homeGraph: homeGraphRouter,
+  ingest: ingestRouter,
   medication: medicationRouter,
   nodeGraph: nodeGraphRouter,
   patient: patientRouter,
   pin: pinRouter,
-  plan: planRouter,
   sim: simRouter,
   simulation: simulationRouter,
-  streak: streakRouter,
   tag: tagRouter,
   twin: twinRouter,
   user: userRouter,
